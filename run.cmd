c450f0441e4e49ad1504d5fa0748b52196c11389
indiamcq <india.mcq@gmail.com>
2017-08-11 15:31:32 +0700

    restructured and new var

--- run.cmd
+++ run.cmd
@@ -1,31 +1,38 @@
 @echo off
 :: Convert to 3gp
 
-:setup
-call :variableslist project.txt
-set errorscheck=0
-if "%ffmpeg-debug%" == "off" (set ffmpeg-info=panic) else (set ffmpeg-info=info)
-set param1=-loglevel %ffmpeg-info% -y -i
-rem set param1=-y -i
-rem set param2=-r 15 -b:v 64k -ac 1 -vf scale=176:132,pad=iw:ih+2*6:0:6 -ar %audio_sample_rate% -b:a %target_bitrate% -acodec libvo_amrwbenc -vcodec h263
-set audio_param=-ac 1 -ar %audio_sample_rate% -b:a %target_bitrate% -strict unofficial -acodec %audio_encoder% 
-set video_param=-r 15 -b:v 64k -vf scale=176:132,pad=iw:ih+2*6:0:6 -vcodec h263
-
-:info
-echo Encoder: %audio_encoder%
-echo Audio sample rate: %audio_sample_rate%. 
-echo Bit rate: %target_bitrate%
-echo.
-
-:checks
-if not exist "%inpath%\%infile_ext%" echo Input %infile_ext% files not found! Check your inpath settings in project.txt. & pause & goto :eof
-if not exist "%ffmpeg%" echo Ffmpeg.exe not found! Check where ffmpeg.exe is installed and correct the path in your project.txt. & pause & goto :eof
-
 :main
-if not exist "%outpath%" md "%outpath%"
+call :variableslist project.txt
+call :checks
+call :variables
+call :info
 for /F usebackq %%m IN (`dir /B "%inpath%\*%infile_ext%"`) DO call :countfiles
 for /F "usebackq delims=" %%m IN (`dir /B "%inpath%\*%infile_ext%"`) DO call :convert "%%m"
 @if '%errorscheck%' gtr '0' Echo Error count = %errorscheck% &pause
+goto :eof
+:variables
+set errorscheck=0
+set param1=-loglevel %ffmpeg_info% -y -i
+set audio_param=-ac 1 -ar %audio_sample_rate% -b:a %target_bitrate% -strict %ffmpeg_strict% -acodec %audio_encoder% 
+set video_param=-r 5 -b:v 24k -vf scale=128:96 -vcodec h263
+goto :eof
+
+:info
+echo FFmpeg audio encoder: %audio_encoder%
+echo Audio sample rate: %audio_sample_rate%. 
+echo Bit rate: %target_bitrate%
+echo Output format: %output_format% %output_format_name%
+echo.
+goto :eof
+
+:checks
+
+if not exist "%outpath%" md "%outpath%"
+if "%output_format%" == "3gp" (set output_format_name=video) else (set output_format_name=audio)
+if "%ffmpeg-debug%" == "off" (set ffmpeg_info=panic) else (set ffmpeg_info=info)
+if "%ffmpeg_strict%" == "" (set ffmpeg_strict=normal)
+if not exist "%inpath%\*%infile_ext%" echo Input %infile_ext% files not found! Check your inpath settings in project.txt. & pause & exit /b
+if not exist "%ffmpeg%" echo Ffmpeg.exe not found! Check where ffmpeg.exe is installed and correct the path in your project.txt. & pause & exit /b
 goto :eof
 
 :countfiles
@@ -37,13 +44,21 @@ goto :eof
 set filename=%~1
 set filename=%filename: =_%
 set infile=%inpath%\%~1
-set outname=%~n1.3gp
+set outname=%~n1.%output_format%
 set /A seqcount=%seqcount%+1
 set outfile=%outpath%\%outname%
 if exist "%outfile%" del "%outfile%"
-"%ffmpeg%" %param1% "%infile%" %video_param% %audio_param% "%outfile%"
+call :%output_format%
 @if exist "%outfile%" echo Created %outname%     %seqcount% of %finalcount% & echo Created %outname%     %seqcount% of %finalcount% >> conversion.log
 @if not exist "%outfile%" echo XXXXXXX File %outname% not created! & set /A errorscheck=%errorscheck%+1& echo XXXXXXX File %outname% not created! >> conversion.log
+goto :eof
+
+:3gp
+"%ffmpeg%" %param1% "%infile%" %video_param% %audio_param% "%outfile%"
+goto :eof
+
+:mp3
+"%ffmpeg%" %param1% "%infile%" %audio_param% "%outfile%"
 goto :eof
 
 :variableslist
